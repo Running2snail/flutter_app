3a30e3680926f1ea46aa773197a103bf85a83873
runningsnail <496668219@qq.com>
2019-07-29 17:22:47 +0800

    Initial commit

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,115 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+const APPBAR_SCROLL_OFFSET = 100;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+
+}
+
+
+class _HomePageState extends State<HomePage>{
+  List _imageUrls = [
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
+  ];
+  double appBarAlpha = 0;
+
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child:NotificationListener(
+                  onNotification: (scrollNotification) {
+                    if (scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0) {
+                        _onScroll(scrollNotification.metrics.pixels);
+                    }
+                  },
+                  child:new ListView(
+                    children: <Widget>[
+                      Container(
+                        height: 240,
+                        child: Swiper(
+                          itemCount: _imageUrls.length,
+                          autoplay: true,
+                          itemBuilder: (BuildContext context, int index){
+                            return Image.network(
+                              _imageUrls[index],
+                              fit: BoxFit.fill,
+                            );
+                          },
+                          pagination: SwiperPagination(),
+                        ),
+                      ),
+                      ListTile(
+                        title: new Text('SwiperController', style: TextStyle(fontWeight: FontWeight.w500),),
+                        subtitle: new Text('iconPrevious'),
+                        leading: new Icon(Icons.account_box, color: Colors.lightBlue),
+                      ),
+                      new Divider(),
+                      ListTile(
+                        title: new Text('SwiperController', style: TextStyle(fontWeight: FontWeight.w500),),
+                        subtitle: new Text('scrollDirection'),
+                        leading: new Icon(Icons.account_box, color: Colors.lightBlue),
+                      ),
+                      new Divider(),
+                      ListTile(
+                        title: new Text('SwiperController', style: TextStyle(fontWeight: FontWeight.w500),),
+                        subtitle: new Text('onIndexChanged'),
+                        leading: new Icon(Icons.account_box, color: Colors.lightBlue),
+                      ),
+                      new Divider(),
+                      Container(
+                        height: 400,
+                        child: ListTile(
+                          title: Text(""),
+                        ),
+                      ),
+                    ],
+                  ),
+              ),
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text(
+                      "首页",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      )
+    );
+  }
+
+}
---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,87 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/Pages/HomePage.dart';
+import 'package:my_app/Pages/MyPage.dart';
+import 'package:my_app/Pages/SearchPage.dart';
+import 'package:my_app/Pages/TravelPage.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+
+}
+
+
+class _TabNavigatorState extends State<TabNavigator>{
+  final PageController _controller = PageController(
+    initialPage: 0,
+
+  );
+
+  final _defaultColor = Colors.grey;
+  final _activityColor = Colors.blue;
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: <Widget>[
+          HomePage(),
+          SearchPage(),
+          TravelPage(),
+          MyPage(),
+
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+          onTap: (index){
+            _controller.jumpToPage(index);
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          items: [
+        BottomNavigationBarItem(
+          icon: Icon( Icons.home, color: _defaultColor, ),
+          activeIcon: Icon( Icons.home, color: _activityColor, ),
+          title: Text(
+            "首页",
+            style: TextStyle(color: _currentIndex == 0 ? _activityColor : _defaultColor),
+          ),
+        ),
+        BottomNavigationBarItem(
+            icon: Icon( Icons.camera_alt, color: _defaultColor, ),
+            activeIcon: Icon( Icons.camera_alt, color: _activityColor, ),
+            title: Text(
+                "旅拍",
+              style: TextStyle(color: _currentIndex == 1 ? _activityColor : _defaultColor),
+            ),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon( Icons.search, color: _defaultColor, ),
+          activeIcon: Icon( Icons.search, color: _activityColor, ),
+          title: Text(
+            "搜索",
+            style: TextStyle(color: _currentIndex == 2 ? _activityColor : _defaultColor),
+          ),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon( Icons.account_circle, color: _defaultColor, ),
+          activeIcon: Icon( Icons.account_circle, color: _activityColor, ),
+          title: Text(
+            "我的",
+            style: TextStyle(color: _currentIndex == 3 ? _activityColor : _defaultColor),
+          ),
+        ),
+      ]),
+    );
+  }
+
+}
